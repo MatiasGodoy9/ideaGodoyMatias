f737c3f47d97398e23b1fa5e80de77f6ed08cf2a
Godoy Matias <pm_godoy@hotmail.com>
2022-08-31 22:33:28 -0300

    agregar contador

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
+
   return (
       <>
       <NavBar />
-      <ItemListContainer />
+      <ItemListContainer texto="Hola Mundo"/>
       </>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import ItemCount from '../ItemCount/ItemCount'
 import Title from '../Title/Title'
 
-const ItemListContainer = () => {
+const ItemListContainer = ({texto}) => {
+
+  const onAdd = (valor) => {
+    console.log(`Compraste ${valor} unidades`)
+  }
+
   return (
-    <Title greeting='Hola Mundo' />
+    <>
+    <Title greeting={texto} />
+    <ItemCount initial={1} stock={10} onAdd={onAdd} />
+    </>
   )
 }
 
